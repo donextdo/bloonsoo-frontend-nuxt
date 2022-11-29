44e549472651573506c8535c0b97f88d98cc7bb4
lakshithadev <lxkshitha@gmail.com>
2022-11-29 12:50:07 +0530

    payments and policies fix

--- pages/listing/hotel/payments.vue
+++ pages/listing/hotel/payments.vue
@@ -11,30 +11,42 @@ const postNext = () => {
 </script>
 
 <template>
+
   <section
     class="md:container mx-auto px-10 py-16 flex flex-col gap-8 text-black font-montserrat"
   >
-    <h2 class="text-2xl font-semibold">List your property on Bloonsoo.com</h2>
+    <h2 class="text-2xl font-semibold">
+      List your property on Bloonsoo.com
+    </h2>
+
     <ListingHotelTabs active_1 active_2 active_3 active_4 active_5 active_6 />
+
     <ListingFormCard label="Guest payment options">
-      <div class="ml-3">
-        <p class="pb-4 text-sm font-bold text-gray-600">
-          Can you charge credit cards at the property?
-        </p>
-        <input type="radio" name="gender" /> <span>Yes</span>
-        <input type="radio" name="gender" class="ml-16" /> <span>No</span>
-        <p class="py-4 font-bold text-gray-600">
-          How do guests book and how do they pay?
-        </p>
-        <p class="text-sm font-bold text-gray-600">
-          To initially secure a reservation we allow guests In use all major
-          credit cards, However. when it comes to collecting payment, you can
-          specify the
-          <span class="block">
+
+      <div class="px-4 flex flex-col gap-6">
+
+        <SharedRadioGroup 
+          title="Can you charge credit cards at the property?" 
+          :options="[{data: 'yes', label: 'yes'}, {data: 'no', label: 'no'}]"
+          name="group3"
+          errorMessage="Please select an option"
+          />
+
+        <div class="flex flex-col gap-2">
+          <p class="text-base font-semibold text-gray-600">
+            How do guests book and how do they pay?
+          </p>
+          <p class="text-sm font-semibold text-gray-600">
+            To initially secure a reservation we allow guests In use all major
+            credit cards, However. when it comes to collecting payment, you can
+            specify the
+            <br>
             payment methods you accept at your property.
-          </span>
-        </p>
+          </p>
+        </div>
+        
       </div>
+
     </ListingFormCard>
 
     <!-- <ListingFormCard label="Commission payments"> -->
@@ -44,14 +56,14 @@ const postNext = () => {
       <div class="grid md:grid-cols-2">
         <h4 class="text-lg text-gray-600 font-semibold">Commission payments</h4>
         <p class="text-end text-md text-gray-600 font-semibold">
-          Commission percentage :<span class="text-xl text-black block"
+          Commission percentage :<span class="text-lg text-black block"
             >15%</span
           >
         </p>
       </div>
 
       <div class="grid gap-6 pl-3 md:grid-cols-2">
-        <div>
+        <div class="flex flex-col gap-6">
           <SharedDropDown
             class="w-11/12"
             label="What name should be placed on the invoice (e.g. legal/company name)?"
@@ -59,64 +71,75 @@ const postNext = () => {
             errorMessage="please enter"
             :options="['John', 'John']"
           />
-          <p class="py-5 pt-6 text-sm font-bold text-gray-700">
-            Does this recipient have the same address as your property?
-          </p>
-          <input type="radio" name="gender" /> <span>Yes</span>
-          <input type="radio" name="gender" class="ml-16" /> <span>No</span>
+
+          <SharedRadioGroup 
+          title="Does this recipient have the same address as your property?" 
+          :options="[{data: 'yes', label: 'yes'}, {data: 'no', label: 'no'}]"
+          name="group4"
+          errorMessage="Please select an option"
+          />
+
         </div>
 
-        <div>
-          <p class="font-bold text-gray-600">
+        <div class="flex flex-col gap-6">
+
+          <h4 class="font-semibold text-base text-gray-600">
             How your commission works for you
-          </p>
-          <div class="py-2 text-sm">
-            <div class="flex items-center">
+          </h4>
+
+          <div class="flex flex-col gap-4 text-sm font-normal">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
-              <p class="py-2 pl-2">No hidden fees—one flat commission rate</p>
+              <p>No hidden fees—one flat commission rate</p>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
-              <p class="py-2 pl-2">Pay only for bookings that stay</p>
+              <p>Pay only for bookings that stay</p>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
 
-              <p class="py-2 pl-2">24/7 personal support by phone or e-mail</p>
+              <p>24/7 personal support by phone or e-mail</p>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
-              <p class="py-2 pl-2">
+              <p>
                 Strong search engine presence for more bookings
               </p>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
-              <p class="py-2 pl-2">
+              <p>
                 Property advice and analytics to increase performance
               </p>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex items-center gap-2">
               <font-awesome-icon
                 icon="fa-circle-check"
                 class="text-green-700 text-md"
               />
-              <p class="py-2 pl-2">Instant confirmations to save you time</p>
+              <p>Instant confirmations to save you time</p>
             </div>
           </div>
         </div>
@@ -125,36 +148,38 @@ const postNext = () => {
     <!-- </ListingFormCard> -->
 
     <ListingFormCard label="Your availability to guests">
-      <div class="ml-3">
-        <p class="py-4 text-sm font-bold text-gray-600">
+      <div class="px-4">
+        <p class="py-4 text-base font-semibold text-gray-600">
           When is the first date that guests can check in?
         </p>
         <p class="text-sm text-justify">
-          <span></span>To help you start earning, we automatically make your
+          To help you start earning, we automatically make your
           property open for bookings for the next 18 months. If you would like
           to make changes to your
-          <span class="block"
-            >availability before opening, you can choose 'complete registration
-            and open later'. Your availability can also be adjusted after you
-            open for bookings.</span
-          >
+          availability before opening, you can choose 'complete registration
+          and open later'. Your availability can also be adjusted after you
+          open for bookings.
         </p>
-        <p class="py-5 pt-8 text-sm font-bold text-gray-600">
+        <p class="py-5 pt-8 text-base font-semibold text-gray-600">
           To complete your registration, please tick the boxes below
         </p>
+
         <div class="flex mb-4">
+
           <input
             id="default-checkbox"
             type="checkbox"
             value=""
             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
           />
+
           <label for="default-checkbox" class="ml-2 text-sm"
             >I certify that this is a legitimate accommodation business with all
             necessary licenses and permits, which can be shown upon first
             request. Booking.com B.V. reserves the right to verify and
             investigate any details provided in this registration.
           </label>
+
         </div>
         <div class="flex mb-4">
           <input
@@ -171,7 +196,7 @@ const postNext = () => {
             and General Delivery Terms.
           </label>
         </div>
-        <p class="py-5 text-sm font-bold text-gray-600">
+        <p class="py-5 text-base font-semibold text-gray-600">
           Almost done! You can always change the information even after you
           completed the registration!
         </p>
@@ -179,13 +204,13 @@ const postNext = () => {
     </ListingFormCard>
     <button
       @click="addRoom"
-      class="w-full py-4 bg-blue-700 text-white font-semibold text-lg rounded-lg hover:bg-blue-900 text-bold"
+      class="w-full py-4 bg-blue-700 text-white font-semibold text-base rounded-lg hover:bg-blue-900 text-bold"
     >
       Complete registration and open tot bookings
     </button>
     <button
       @click="addRoom"
-      class="w-full px-4 py-4 font-semibold text-lg text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent text-bold"
+      class="w-full px-4 py-4 font-semibold text-base text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent text-bold"
     >
       Complete Registration and open later
     </button>
--- pages/listing/hotel/policies.vue
+++ pages/listing/hotel/policies.vue
@@ -6,7 +6,7 @@ definePageMeta({
 const router = useRouter();
 
 const postNext = () => {
-  router.push({ path: "/listing/hotel/policies" });
+  router.push({ path: "/listing/hotel/payments" });
 };
 
 
@@ -20,7 +20,9 @@ const postNext = () => {
       List your property on Bloonsoo.com
     </h2>
     <ListingHotelTabs active_1 active_2 active_3 active_4 active_5 />
+
     <ListingFormCard label="Cancellations">
+
       <div class="grid grid-cols-2 gap-x-8 gap-y-6 px-4">
 
         <SharedDropDown
@@ -46,13 +48,14 @@ const postNext = () => {
             'No reservation needed',
           ]"
         />
+
       </div>
-      <div class="ml-4">
-        <p class="py-2 text-md font-bold text-gray-600">
+      <div class="flex flex-col gap-2 px-4">
+        <p class="text-sm font-semibold text-gray-600">
           The guest must cancel by 18:00 on the day of arrival or pay 100% of
           the price for the fit st night.
         </p>
-        <p class="text-md">
+        <p class="text-sm font-semibold text-gray-500">
           Please note. You'll be able to make changes to your policies later on.
           this is just to get you started.
         </p>
@@ -60,35 +63,43 @@ const postNext = () => {
     </ListingFormCard>
 
     <ListingFormCard label="Protect against accidental bookings">
+
       <div class="grid grid-cols-2 gap-x-8 gap-y-6 px-4">
-        <div class="inline-flex items-center">
-          <span class="text-sm font-medium mr-11 dark:text-gray-600">Yes</span>
+
+        <div class="flex items-center gap-4">
+
+          <span class="text-sm font-semibold text-gray-600">Yes</span>
           <label class="relative cursor-pointer ml-5">
+
             <input type="checkbox" value="" class="sr-only peer" />
+
             <div
-              class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+              class="w-11 h-6 bg-gray-200 focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
             ></div>
+
           </label>
+
         </div>
       </div>
 
-      <div class="ml-4">
-        <p class="text-sm text-gray-600 font-bold">
+      <div class="px-4">
+        <p class="text-sm text-gray-600 font-semibold">
           To save your time handling accidental bookings, we automatically wave
           the cancellation fees for the guests who cancel the booking
-          <span class="block">
-            within the first 2 hours after made the booking. You can change this
-            period of time later in your property management platform.
-          </span>
+          <br>
+          within the first 2 hours after made the booking. You can change this
+          period of time later in your property management platform.
         </p>
       </div>
     </ListingFormCard>
 
-    <div class="grid md:grid-cols-2 gap-3">
-      <div class="grid bg-gray-100 rounded-md">
-        <ListingFormCard label="Check in">
+    <div class="grid md:grid-cols-2 gap-6">
+
+
+      <ListingFormCard label="Check in">
+        <div class="px-4 flex flex-col gap-6">
+
           <SharedDropDown
-            class="pl-3"
             label="From"
             v-model="roomType"
             errorMessage="please enter"
@@ -96,18 +107,22 @@ const postNext = () => {
           />
 
           <SharedDropDown
-            class="pl-3"
             label="Untill"
             v-model="roomType"
             errorMessage="please enter"
             :options="['18:00', '19:00', '20:00']"
           />
-        </ListingFormCard>
-      </div>
-      <div class="grid bg-gray-100 rounded-md">
-        <ListingFormCard label="Check out">
+
+        </div>
+          
+      </ListingFormCard>
+
+      
+      <ListingFormCard label="Check out">
+
+        <div class="px-4 flex flex-col gap-6">
+
           <SharedDropDown
-            class="pl-3"
             label="From"
             v-model="roomType"
             errorMessage="please enter"
@@ -115,24 +130,27 @@ const postNext = () => {
           />
 
           <SharedDropDown
-            class="pl-3"
             label="Untill"
             v-model="roomType"
             errorMessage="please enter"
             :options="['18:00', '19:00', '20:00']"
           />
-        </ListingFormCard>
-      </div>
+
+        </div>
+          
+      </ListingFormCard>
     </div>
 
     <ListingFormCard label="Children and Pets">
-      <div class="ml-3">
-        <p class="pb-3 text-sm text-gray-600 font-bold">
-          Children Can you accommodate children? (You can specify the ages and
-          prices later)
-        </p>
-        <input type="radio" name="gender" /> <span>Yes</span>
-        <input type="radio" name="gender" class="ml-16" /> <span>No</span>
+      <div class="px-4">
+
+        <SharedRadioGroup 
+          title="Children Can you accommodate children? (You can specify the ages and
+          prices later)"
+          :options="[{data: 'yes', label: 'yes'}, {data: 'no', label: 'no'}]"
+          name="children"
+          errorMessage="Please select an option"
+          />
         
                    
         <SharedDropDown
@@ -142,13 +160,17 @@ const postNext = () => {
           errorMessage="please enter"
           :options="['Yes', 'No']"
         />
+
       </div>
+
     </ListingFormCard>
+
     <button
-      @click="addRoom"
+      @click="postNext"
       class="w-full py-4 bg-blue-700 text-white font-semibold text-base rounded-lg hover:bg-blue-900"
     >
       Next
     </button>
+
   </section>
 </template>
