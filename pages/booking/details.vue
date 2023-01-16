1ba281ad8a1a0fc1111f6a783baeb3d17566894c
lakshithadev <lxkshitha@gmail.com>
2023-01-16 13:26:50 +0530

    change styles

--- pages/booking/details.vue
+++ pages/booking/details.vue
@@ -33,13 +33,9 @@ const baseUrl = config.public.baseUrl
 
                 </div>
 
-                <!-- <BookingDetailsInfo /> -->
-
                 <ListingFormCard label="Enter your details">
 
-                    <div class="grid grid-cols-2 gap-x-8 gap-y-6 px-4">
-
-
+                    <div class="grid grid-cols-2 gap-x-16 gap-y-6 px-4">
 
                         <SharedRadioGroup 
                         title="Are you traveling for work?"
@@ -51,123 +47,115 @@ const baseUrl = config.public.baseUrl
                         errorMessage="Please select an option"
                         />
 
-
-                        <div class="flex flex-col gap-2 items-start  col-start-1">
-
                         <SharedTextInput label="First Name" v-model="firstName" :error="firstNameError" errorMessage="First Name cannot be empty" placeholder="Regina" />
-
-
-                        </div>
-
-                        <div class="flex flex-col gap-2 items-start col-start-2">
 
                         <SharedTextInput label="Last Name" v-model="lastName" :error="lastNameError" errorMessage="Last Name cannot be empty" placeholder="George" />
 
+                        <div class="flex flex-col gap-2">
+                            <SharedTextInput label="Email Address" v-model="email" :error="emailError" errorMessage="Email Address cannot be empty" placeholder="Regina@fun.com" />
+
+                            <p class="text-sm text-gray-500">
+                                Confirmation email sent to this address 
+                            </p>
                         </div>
-
-                    </div>
-
-                    <SharedTextInput label="Email Address" v-model="email" :error="emailError" errorMessage="Email Address cannot be empty" placeholder="Regina@fun.com" class="w-3/5 px-4" />
-
-                    <p class="text-sm text-gray-500 px-4">
-                        This name will be seen to guests
-                    </p>
-
-                    <SharedTextInput label="Confirm Email Address" v-model="email" :error="emailError" errorMessage="Email Address cannot be empty" placeholder="Regina@fun.com" class="w-3/5 px-4" />
-
-
-                            
-                        <p class=" mx-4 text-sm font-bold text-gray-700">Who are you booking for?</p>
-                        <div class="flex items-center  mx-8">
-                            <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="default-radio-1" class="ml-2 text-sm"> I'm the main guest</label>
-                        </div>
-                        <div class="flex items-center mx-8">
-                            <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="default-radio-2" class="ml-2 text-sm "> I'm booking for someone else </label>
-                        </div>
-
-                                
                         
+                        <SharedTextInput label="Confirm Email Address" v-model="email" :error="emailError" errorMessage="Email Address cannot be empty" placeholder="Regina@fun.com" class="col-start-1" />
 
+                        <SharedRadioGroup 
+                        title="Who are you booking for?"
+                        class="col-start-1" 
+                        v-model="ownMultipleHotels" 
+                        :options="[
+                            {data: 'yes', label: `I'm the main guest`}, 
+                            {data: 'no', label: ` I'm booking for someone else `}
+                        ]"
+                        :error="ownMultipleHotelsError"
+                        name="group1"
+                        errorMessage="Please select an option"
+                        vertical
+                        />
 
+                    </div>
 
-                    </ListingFormCard>
+                </ListingFormCard>
 
-                    <ListingFormCard label="Standard Double City View with Early Check-in at 11 AM & Late Check-out at 3 PM (on Availability)">
+                <ListingFormCard label="Standard Double City View with Early Check-in at 11 AM & Late Check-out at 3 PM (on Availability)">
 
-                    <div class="px-6 mb-6">
-                        <div class="w-full flex items-center mb-2">
-                            <font-awesome-icon icon="fa fa-ban" class="text-gray-700 text-xl mx-6"/>
-                            <p>Total cost to cancel</p>
+                    <div class="flex flex-col gap-8 px-4">
+
+                        <div class="flex flex-col gap-2">
+                            <div class="w-full flex items-center gap-3">
+                                <font-awesome-icon icon="fa fa-ban" class="text-gray-700 text-xl"/>
+                                <p class="text-gray-500 font-semibold">Total cost to cancel</p>
+                            </div>
+
+                            <div class="w-full flex items-center gap-3">
+                                <IconsSparkle class="w-5 h-5"/>
+                                <p class="text-gray-500">Sparkly clean rooms</p>
+                            </div>
+
+                            <div class="w-full flex items-center gap-3">
+                                <font-awesome-icon icon="fa-solid fa-ban-smoking" class="text-gray-500 text-xl" /> 
+                                <p class="text-gray-500">No smoking </p>
+                            </div>
                         </div>
 
-                        <div class="w-full flex items-center mb-2 mx-6">
-                            <HotelFacility class="!p-0 !w-max mr-3" icon="sparkle"/> 
-                            <p class="text-gray-500">Sparkly clean rooms  </p>
+                        <div class="w-full flex items-center justify-between">
+                            <HotelFacility class="!p-0 !w-max" icon="kitchen" label="Kitchen"/>
+                            <HotelFacility class="!p-0 !w-max" icon="ac" label="Air Conditioner"/>
+                            <HotelFacility class="!p-0 !w-max" icon="washer" label="Washer"/>
+                            <HotelFacility class="!p-0 !w-max" icon="kingbed" label="King size bed"/>
+                            <HotelFacility class="!p-0 !w-max" icon="cleaning" label="Cleaning Service"/>
                         </div>
 
-                        <div class="w-full flex items-center mb-2">
-                            <font-awesome-icon icon="fa-solid fa-ban-smoking" class="text-gray-500 text-xl mx-6" /> 
-                            <p class="text-gray-500">No smoking </p>
+                        <div class="flex flex-col gap-4">
+                            <h4 class="text-sm font-semibold text-gray-600">
+                                Your Genius benefits
+                            </h4>
+
+                            <div class="flex gap-4 items-start">
+                                <font-awesome-icon icon="fa-solid fa-circle-check" class="text-green-600 text-xl" />
+                                <p class="text-sm text-gray-600">
+                                    <span class="text-black font-semibold block">10% discount</span>
+                                    You're getting a 10% discount on the price of this option before taxes and fees apply.
+                                </p>
+                            </div>
                         </div>
 
+                        <p class="text-sm font-semibold text-gray-600">
+                            Guests: 2 adults
+                        </p>
+
+                        <div class="grid grid-cols-2 gap-x-16">
+                            <SharedTextInput label="Full guest name" v-model="guestName" :error="guestNameError" errorMessage="Full guest name cannot be empty" placeholder="Regina George" />
+                        </div>
+                        
                     </div>
 
-                    <div class="w-full flex items-center justify-between mt-auto mb-6 px-6">
-                        <HotelFacility class="!p-0 !w-max" icon="kitchen" label="Kitchen"/>
-                        <HotelFacility class="!p-0 !w-max" icon="ac" label="Air Conditioner"/>
-                        <HotelFacility class="!p-0 !w-max" icon="washer" label="Washer"/>
-                        <HotelFacility class="!p-0 !w-max" icon="kingbed" label="King size bed"/>
-                        <HotelFacility class="!p-0 !w-max" icon="cleaning" label="Cleaning Service"/>
-                    </div>
+                </ListingFormCard>
 
-                    <div>
-                    <p class="text-sm font-semibold text-gray-600 px-4 mb-6">
-                    Your Genius benefits
-                    </p>
-
-                    <div class=" flex row px-6 mb-2">
-                        <font-awesome-icon icon="fa-solid fa-circle-check" class="text-green-600 text-xl mx-6" />
-                    <p class=" text-sm font-semibold text-gray-600">10% discount</p>
-                    </div>
-
-                    <p class="mx-24 text-sm ">You're getting a 10% discount on the price of this option before taxes and fees apply. </p>
-                    </div>
-
-                    <p class="text-sm font-semibold text-gray-600 px-4 mb-6">
-                    Guests: 2 adults
-                    </p>
-
-                    <SharedTextInput label="Full guest name" v-model="guestName" :error="guestNameError" errorMessage="Full guest name cannot be empty" placeholder="Regina George" class="w-3/5 px-4" />
-
-
-                    </ListingFormCard>
-
-                    <ListingFormCard label="Your arrival time ">
+                <ListingFormCard label="Your arrival time ">
 
                     <div class="px-4 flex flex-col gap-6">
 
-                        <div class="w-full grid grid-cols-2 gap-6">
+                        <div class="w-full grid grid-cols-2 gap-x-16">
 
                             <SharedDropDown 
                             label="Add your estimated arrival time (optional) " 
                             v-model="country"
-                            :options="['9:00 AM — 10:00 AM ', '10:00 AM — 11:00 AM ', '11:00 AM — 12:00 AM ']" 
+                            :options="['9:00 AM — 10:00 AM', '10:00 AM — 11:00 AM', '11:00 AM — 12:00 AM']" 
                             class="col-start-1" />
 
                         </div>
                     </div>
 
-                    </ListingFormCard>
+                </ListingFormCard>
 
             </section>
 
             <aside class="w-full col-span-1 h-full flex flex-col gap-4">
 
-                <!-- <BookingCheckIn /> -->
-
-                <div class="  shadow-md  w-full p-5 mb-4">
+                <div class="shadow-md  w-full p-5 mb-4">
                     <p class="font-semibold mb-8 text-gray-700 text-md">Your booking details  </p>
 
                     <div class="grid grid-cols-2 gap-1 mb-5 ">
